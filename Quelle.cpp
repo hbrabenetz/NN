dadd15bf66d09a483e214c85706d1c7adc2bf29b
hbrabenetz <hbrabenetz@tgm.ac.at>
2018-02-23 06:16:16 -0500

    Update Quelle.cpp

--- Quelle.cpp
+++ Quelle.cpp
@@ -1,20 +1,10 @@
 
 #include "N.h" 
 
-//double nor(double x) { return x; }
-//double den(double x) { return x; }
 
 int main() {
 
 	N n{ topologie{ 2,3,1 }, 0.9, activationMethodchoosen::eins_durch_ehoch, {1.0, 0.0, 1.0, 0.0} };
-	
-
-	//auto [Max, Min, max, min] = n.normalizationParam; // this is how i can get the normalization Paramters out again
-
-
-	//	auto nor = std::bind(normalizationfunction1, std::placeholders::_1, 1.0, 0.0, 1.0, 0.0);
-	//	auto den = std::bind(denormalizationfunction1, std::placeholders::_1, 1.0, 0.0, 1.0, 0.0);
-
 
 	// or on the heap
 	// N* pn = new N{ topologie{ 2,3,1 }, 0.9, activationMethodchoosen::eins_durch_ehoch };
@@ -22,7 +12,7 @@ int main() {
 
 	std::chrono::high_resolution_clock::time_point timex;
 	std::chrono::nanoseconds elapsed;
-	// … 1 sec == 1 000 000 000 Nanosekunden
+	// 1 sec == 1 000 000 000 Nanosekunden
 
 	timex = std::chrono::high_resolution_clock::now();
 	if (1) // proper sequence for teaching
@@ -43,36 +33,35 @@ int main() {
 			n.nod[0][1] = 1.0;
 			n.tru[0] = 0.0;
 			n.calc();
-		}
+		} // for (int it = 0; it < 1000; ++it) {
 	elapsed = std::chrono::high_resolution_clock::now() - timex;
 
 
-	if (1) {
+	if (1) { // check if the network really learned to solve its task
 		cout << "Ergebnis:" << endl;
 		n.nod[0][0] = 0.0;
 		n.nod[0][1] = 0.0;
 		n.tru[0] = 0.0;
 		n.calc();
-		cout << n.nod[0][0] << n.nod[0][1] << " -> " << n.tru[0] << " return = " << (n.getCalcRes())[0] << endl; // << den(n.nod[n.Nlay - 1][0]) << endl; // den(n.calc()) << endl;
+		cout << n.nod[0][0] << n.nod[0][1] << " -> " << n.tru[0] << " return = " << (n.getCalcRes())[0] << endl;
 		n.nod[0][0] = 0.0;
 		n.nod[0][1] = 1.0;
 		n.tru[0] = 1.0;
 		n.calc();
-		cout << n.nod[0][0] << n.nod[0][1] << " -> " << n.tru[0] << " return = " << (n.getCalcRes())[0] << endl; // << den(n.nod[n.Nlay - 1][0]) << endl; // den(n.calc()) << endl;
+		cout << n.nod[0][0] << n.nod[0][1] << " -> " << n.tru[0] << " return = " << (n.getCalcRes())[0] << endl;
 		n.nod[0][0] = 1.0;
 		n.nod[0][1] = 0.0;
 		n.tru[0] = 1.0;
 		n.calc();
-		cout << n.nod[0][0] << n.nod[0][1] << " -> " << n.tru[0] << " return = " << (n.getCalcRes())[0] << endl; // << den(n.nod[n.Nlay - 1][0]) << endl; // den(n.calc()) << endl;
+		cout << n.nod[0][0] << n.nod[0][1] << " -> " << n.tru[0] << " return = " << (n.getCalcRes())[0] << endl;
 		n.nod[0][0] = 1.0;
 		n.nod[0][1] = 1.0;
 		n.tru[0] = 0.0;
 		n.calc();
-		cout << n.nod[0][0] << n.nod[0][1] << " -> " << n.tru[0] << " return = " << (n.getCalcRes())[0] << endl; // den(n.nod[n.Nlay - 1][0]) << endl; // den(n.calc()) << endl;
-	}
+		cout << n.nod[0][0] << n.nod[0][1] << " -> " << n.tru[0] << " return = " << (n.getCalcRes())[0] << endl;
+	} // if (1) { // check if the network really learned to solve its task
 
 	std::cout << "Elapsed time in seconds = " << elapsed.count() / 1000000000.0 << std::endl;
-
 
 	getchar();
 	return 0;
